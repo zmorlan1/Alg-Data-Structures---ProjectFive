a4855ed7de6213f5711b8c9433bb0a7b3669998d
zmorlan1 <52795205+zmorlan1@users.noreply.github.com>
2019-07-21 22:20:00 -0600

    Update MbamiLuka_05.java
    changed while loop

--- MbamiLuka_05.java
+++ MbamiLuka_05.java
@@ -8,14 +8,53 @@
 public class MbamiLuka_05 
 {
 
-	public static void main(String[] args) 
-	{
-		Postfix expression = new Postfix();
-		
-		// testing
-		System.out.println
-		(expression.convertToPostfix ("a+b*(c^d-e)^(f+g*h)-i"));
+	public static void main ( String[] args) throws FileNotFoundException {
+		Postfix expression;
 
-	}
+		final String INPUT_FILENAME; // Name of inputFile
+		INPUT_FILENAME = "Project05_Input.txt";
+
+		File inputFile = new File ( INPUT_FILENAME); // New input file
+		Scanner input = new Scanner ( inputFile); // Scanner reading file
+
+		String div = "_____________________________________________" + 
+					 "_____________________________________________" ;
+
+		// Print Header and a divider
+		System.out.printf ( "%-51s %35s\n", "INFIX", "POSTFIX");
+		System.out.println ( div);
+
+		// Read each input line and convert to its Postfix expression
+		while  ( input.hasNextLine ( )) 
+		{
+			String lineRead = input.nextLine ( );
+        	        String lineFormatted = lineRead.trim();
+
+			expression = new Postfix ( );
+			String exp;
+
+			if ( expression.parenthesesCheck( lineFormatted ) == false) 
+			{
+				exp = "None - Uneven number of parentheses";	
+			}
+         else if ( expression.operandCheck ( lineFormatted) == false)
+				{
+					exp = "None - Adjacent operands";
+				} 
+			else 
+			{
+				exp = expression.convertToPostfix ( lineFormatted);
+			}
+			
+
+			System.out.printf ( "\n%-52s %37s\n", lineRead, exp);
+			System.out.println ( div);
+
+		} // End while loop reading lines
+
+		input.close ( );
+
+	} // End main
+}
 
 }
